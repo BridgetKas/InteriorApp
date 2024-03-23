bdceda006e6d4adf9e1acaa5c4b311adfea00474
BridgetKas <bridgetnamugga@gmail.com>
2024-03-23 22:23:00 +0900

    Added Nav Bar

--- src/components/common/Nav/NavBar.jsx
+++ src/components/common/Nav/NavBar.jsx
@@ -1,22 +1,32 @@
 import React from 'react'
 import { NavLink } from 'react-router-dom'
-import "./NavBar.module.css"
+import sytles from "./NavBar.module.css"
+import { PiShoppingCartSimple } from "react-icons/pi";
+import { MdPersonOutline } from "react-icons/md";
+import { colors } from '../../../utils/colors';
 
 const menu = [
     { path: '/', label: "Home" },
     { path: '/about', label: "About us" },
     { path: '/blog', label: "Blog" },
-    { path: '/cart', label: "Cart" },
     { path: '/services', label: "Services" },
     { path: '/shop', label: "Shop" },
-    { path: '/cart', label: "Cart" },
     { path: '/contact', label: "Contact us" },
 ]
 
 function NavBar() {
   return (
-    <div>
-        {menu.map(item => <NavLink to={item.path} className="menuItem">{item.label}</NavLink>)}
+    <div className={sytles.mainContainer}>
+        <div className={sytles.logoText}>Dream</div>
+        <div className={sytles.menuContainer}>
+          <div className={sytles.menu}>
+            {menu.map(item => <NavLink to={item.path} key={item.path} className="menuItem">{item.label}</NavLink>)}
+          </div>
+          <div className={sytles.menu}>
+            <MdPersonOutline size={30} color={colors.whiteGradient}/>
+            <NavLink to="/cart"><PiShoppingCartSimple size={30}/></NavLink>
+          </div>
+        </div>
     </div>
   )
 }
