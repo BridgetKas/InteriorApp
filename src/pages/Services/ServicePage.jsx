5826582e318ca68f8dc00fb266b931b693727fad
BridgetKas <bridgetnamugga@gmail.com>
2024-04-14 17:36:54 +0900

    Added Service  Page

--- src/pages/Services/ServicePage.jsx
+++ src/pages/Services/ServicePage.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
 import "./Service.module.css"
+import Header from '../../layout/Header/Header'
+import greenCouch from "../../assets/home/couch.png"
+
 function Service() {
   return (
-    <div>Service</div>
+    <div>
+       <Header title="Services" imageSrc={greenCouch}titleDescription="Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique."/>
+    </div>
   )
 }
 
