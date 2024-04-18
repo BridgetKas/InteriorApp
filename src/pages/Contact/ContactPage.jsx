849e0926e72f2514aae816a1d4242494c54515c3
BridgetKas <bridgetnamugga@gmail.com>
2024-04-18 21:47:48 +0900

    Added Contact  Page

--- src/pages/Contact/ContactPage.jsx
+++ src/pages/Contact/ContactPage.jsx
@@ -2,12 +2,13 @@ import React from 'react'
 import"./Contact.module.css"
 import Header from '../../layout/Header/Header'
 import greenCouch from "../../assets/home/couch.png"
+import Contact from './contact'
 
 function ContactPage() {
   return (
     <div>
        <Header title="Contact" imageSrc={greenCouch}titleDescription="Donec vitae odio quis nisl dapibus malesuada. Nullam ac aliquet velit. Aliquam vulputate velit imperdiet dolor tempor tristique."/>
-
+        <Contact/>
 
     </div>
   )
--- src/pages/Contact/contact.jsx
+++ src/pages/Contact/contact.jsx
@@ -3,33 +3,34 @@ import { FaLocationDot } from "react-icons/fa6";
 import { MdEmail } from "react-icons/md";
 import { FaPhone } from "react-icons/fa6";
 import style from "./Contact.module.css"
-
+import {colors} from"../../utils/colors";
 
 const contacts = [
     {
-        icon:<FaLocationDot />,
+        icon:<FaLocationDot color={colors.white} size={17}/>,
         information:"43 Raymouth Rd. Baltemoer, London 3910"
     },
     {
-        icon:<MdEmail />,
+        icon:<MdEmail color={colors.white} size={17}/>,
         information:"info@yourdomain.com"
     },
     {
-        icon:<FaPhone />,
+        icon:<FaPhone color={colors.white} size={17}/>,
         information:"+1 294 3925 3939"
     }
 ]
 
-function contact() {
+function Contact() {
   return (
     <div className={style.mainContactContainer}>
-        {contacts.map((contact) => {
-            return <div key={index} className={style.contactContainer}></div>
+        {contacts.map((contact,index) => {
+            return <div key={index} className={style.contactContainer}>
+                <div className={style.iconContainer}>{contact.icon}</div>
+                <div className={style.contactInformation}>{contact.information}</div>
+            </div>
         })}
-
-
     </div>
   )
 }
 
-export default contact
+export default Contact
